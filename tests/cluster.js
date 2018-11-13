3959468f36f67abce73f82d149247f2877820b6c
Rafal Okninski <hpn777@gmail.com>
2018-11-13 12:17:02 +0000

    NATS racing condition on sturtup fix

--- tests/cluster.js
+++ tests/cluster.js
@@ -4,62 +4,68 @@ const TessCluster = require('../lib/cluster')
 
 
 var node1 = new TessCluster()
+let node2 = new TessCluster()
+
 node1.connect({clientName: 'client1'})
     .then((nc) => {
-        console.log('client1 online')
-        var users1 = node1.createTesseract('users', usersDefinition)
-        var tess1 = node1.createTesseract('messageQueue', messageQueueDefinition)
-        
-        users1.add([{id: 1, name: 'rafal'},{id: 2, name: 'daniel'},{id: 3, name: 'lauren'}])
+        console.log('node1 online')
+        node1.createTesseract('users', usersDefinition)
+            .then((users) => {
+                users.add([{id: 1, name: 'rafal'},{id: 2, name: 'daniel'},{id: 3, name: 'lauren'}])
+            })
 
-        let ii = 1
-        tess1.add({id: ii++, message: 'dupa', user: 3, status: 1})
-        tess1.add({id: ii++, message: 'cipa', user: 1, status: 1})
-        tess1.add({id: ii++, message: 'bla', user: 2, status: 1})
-        tess1.add({id: ii++, message: 'bla2', user: 2, status: 2})
-        tess1.add({id: ii++, message: 'bla3', user: 2, status: 2})
+        node1.createTesseract('messages', messageQueueDefinition)
+            .then((messages) => {
+                let ii = 1
+                messages.add({id: ii++, message: 'dupa', user: 3, status: 1})
+                messages.add({id: ii++, message: 'cipa', user: 1, status: 1})
+                messages.add({id: ii++, message: 'bla', user: 2, status: 1})
+                messages.add({id: ii++, message: 'bla2', user: 2, status: 2})
+                messages.add({id: ii++, message: 'bla3', user: 2, status: 2})
 
-        tess1.update({id: 2, message: 'cipa2', status: 2})
-        tess1.update([{id: 5, message: 'retretrt', status: 1}, {id: 4, message: 'cipa2', status: 2}])
+                messages.update({id: 2, message: 'cipa2', status: 2})
+                messages.update([{id: 5, message: 'retretrt', status: 1}, {id: 4, message: 'cipa3', status: 2}])
+                console.log('sent shit --------------')
+            })
     })
 
-let node2 = new TessCluster()
 node2.connect({clientName: 'client2'})
     .then((nc) => {
-        console.log('client2 online')
-        node2.on('add', (tess)=>{
-            if(tess.get('id') === 'messageQueue'){
-                let session = node2.createSession({
-                    id: 'messages_querry',
-                    table: 'messageQueue',
-                    columns:  [{
-                        name: 'id',
-                        primaryKey: true
-                    },{
-                        name: 'message',
-                    },{
-                        name: 'userName',
-                        resolve: {
-                            underlyingName: 'user',
-                            childrenTable: 'users',
-                            valueField: 'id',
-                            displayField: 'name'
-                        }
-                    }],
-                    filter: [{
-                        type: 'custom',
-                        value: 'status == 2',
-                    }],
-                    sort: [{ property: 'id', direction: 'DESC' }]
-                })
-                
-                session.on('dataUpdated',(data)=>{
-                    console.log('dataUpdated', data.toJSON())
-                })
-            }
-        })
+        console.log('node2 online')
     })
 
+node2.on('add', (tess)=>{
+    console.log(tess.get('id') + ' added on node 2')
+    if(tess.get('id') === 'messages'){
+        let session = node2.createSession({
+            id: 'messages_querry',
+            table: 'messages',
+            columns:  [{
+                name: 'id',
+                primaryKey: true
+            },{
+                name: 'message',
+            },{
+                name: 'userName',
+                resolve: {
+                    underlyingName: 'user',
+                    childrenTable: 'users',
+                    valueField: 'id',
+                    displayField: 'name'
+                }
+            }],
+            filter: [{
+                type: 'custom',
+                value: 'status == 2',
+            }],
+            sort: [{ property: 'id', direction: 'DESC' }]
+        })
+        
+        session.on('dataUpdated',(data)=>{
+            console.log('dataUpdated', data.toJSON())
+        })
+    }
+})
 let messageQueueDefinition = {
     clusterSync: true,
     isDurableStream: true,
@@ -105,7 +111,7 @@ var usersDefinition = {
 
 
 
-setTimeout(()=>{
-    console.log('summary',node2.getSession('messages_querry').getData().map(x=>x.object))
+// setTimeout(()=>{
+//     console.log('summary',node2.getSession('messages_querry').getData().map(x=>x.object))
     
-}, 2000)
+// }, 2000)
--- tests/unit/cluster.js
+++ tests/unit/cluster.js
@@ -7,37 +7,41 @@ const TessCluster = require('../../lib/cluster')
 
 tape('Tesseract cluster test', t => {
     var node1 = new TessCluster()
+    let node2 = new TessCluster()
+
     node1.connect({clientName: 'client1'})
         .then((nc) => {
-            var users1 = node1.createTesseract('users', usersDefinition)
-            var tess1 = node1.createTesseract('messageQueue', messageQueueDefinition)
-            
-            users1.add([{id: 1, name: 'rafal'},{id: 2, name: 'daniel'},{id: 3, name: 'lauren'}])
+            node1.createTesseract('users', usersDefinition)
+                .then((users) => {
+                    users.add([{id: 1, name: 'rafal'},{id: 2, name: 'daniel'},{id: 3, name: 'lauren'}])
+                })
 
-            let ii = 1
-            tess1.add({id: ii++, message: 'dupa', user: 3, status: 1})
-            tess1.add({id: ii++, message: 'cipa', user: 1, status: 1})
-            tess1.add({id: ii++, message: 'bla', user: 2, status: 1})
-            tess1.add({id: ii++, message: 'bla2', user: 2, status: 2})
-            tess1.add({id: ii++, message: 'bla3', user: 2, status: 2})
+            node1.createTesseract('messages', messageQueueDefinition)
+                .then((messages) => {
+                    let ii = 1
+                    messages.add({id: ii++, message: 'dupa', user: 3, status: 1})
+                    messages.add({id: ii++, message: 'cipa', user: 1, status: 1})
+                    messages.add({id: ii++, message: 'bla', user: 2, status: 1})
+                    messages.add({id: ii++, message: 'bla2', user: 2, status: 2})
+                    messages.add({id: ii++, message: 'bla3', user: 2, status: 2})
 
-            tess1.update({id: 2, message: 'cipa2', status: 2})
-            tess1.update([{id: 5, message: 'retretrt', status: 1}, {id: 4, message: 'cipa3', status: 2}])
+                    messages.update({id: 2, message: 'cipa2', status: 2})
+                    messages.update([{id: 5, message: 'retretrt', status: 1}, {id: 4, message: 'cipa3', status: 2}])
+                })
         })
 
-    let node2 = new TessCluster()
     node2.connect({clientName: 'client2'})
     
     node2.on('add', (tess)=>{
-        if(tess.get('id') === 'messageQueue'){
+        if(tess.get('id') === 'messages'){
             let session = node2.createSession(messagesSession)
             
             setTimeout(()=>{
                 let data = session.getData().map(i => i.object)
-                    assertArraysMatch(data, dataResult, e => t.fail(e), () => t.pass('Data OK'))
-                    node1.close()
-                    node2.close()
-                    t.end()
+                assertArraysMatch(data, dataResult, e => t.fail(e), () => t.pass('Data OK'))
+                node1.close()
+                node2.close()
+                t.end()
             }, 200)
         }
     })
@@ -69,8 +73,8 @@ tape('Tesseract cluster test', t => {
             }
         }]
     }
-
-    let usersDefinition = {
+    
+    var usersDefinition = {
         clusterSync: true,
         isDurableStream: true,
         columns: [{
@@ -78,13 +82,12 @@ tape('Tesseract cluster test', t => {
             primaryKey: true,
         }, {
             name: 'name',
-            columnType: 'text',
         }]
     }
 
     let messagesSession = {
         id: 'messagesSession',
-        table: 'messageQueue',
+        table: 'messages',
         columns:  [{
             name: 'id',
             primaryKey: true
