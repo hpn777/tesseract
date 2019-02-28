91e863052f3413e7a4e0b928ede98a93a099c9e2
Rafal Okninski <hpn777@gmail.com>
2019-02-28 09:59:20 +0000

    expressionEngine.js changes isin -> in

--- tests/expressionEngine.js
+++ tests/expressionEngine.js
@@ -1,6 +1,6 @@
  const { ExpressionEngine } = require('../lib/expressionEngine')
 
 let ee = new ExpressionEngine()
-let expression = ee.generateExpressionTree('a isin (1,5,8)')
+let expression = ee.generateExpressionTree('a in (1,5,8)')
 
-console.log(ee.executeExpressionTree(expression, {a: 8}))
+console.log(ee.executeExpressionTree(expression, {a: 2}))
