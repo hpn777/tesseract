4e85ad450f3c74fbe8645d697ff4c234e05178ba
Wiktor Toporek <wiktor@tsh.io>
2019-05-29 14:41:10 +0200

    include columnReason in LiveQuery unit tests

--- tests/unit/tesseract.js
+++ tests/unit/tesseract.js
@@ -41,16 +41,20 @@ tape('LiveQuery test', t => {
 
     let result = [{
         'addedIds': [1],
-        'removedIds': []
+        'removedIds': [],
+        'updateReason': 'dataUpdate'
     }, {
         'addedIds': [2],
-        'removedIds': []
+        'removedIds': [],
+        'updateReason': 'dataUpdate'
     }, {
         'addedIds': [3],
-        'removedIds': []
+        'removedIds': [],
+        'updateReason': 'dataUpdate'
     }, {
         'addedIds': [4],
-        'removedIds': []
+        'removedIds': [],
+        'updateReason': 'dataUpdate'
     }, {
         'addedIds': [10],
         'removedIds': []
@@ -78,7 +82,7 @@ tape('LiveQuery test', t => {
         },
         e => { console.trace(e) }
     )
-        
+
 
     messages.add({id: 1, message: 'lorem', status: 1})
     messages.add({id: 2, message: 'ipsum', status: 1})
@@ -98,3 +102,57 @@ tape('LiveQuery test', t => {
     ])
 })
 
+tape('Live query columns update test', t => {
+  let result = [{
+    'addedIds': [2],
+    'removedIds': [],
+    'updateReason': 'dataUpdate'
+  }, {
+    'addedIds': [3],
+    'removedIds': [],
+    'updateReason': 'dataUpdate'
+  }, {
+    'updatedIds': [2, 3],
+    'updateReason': 'columnsChanged',
+  }]
+
+  var messages = new Tesseract({
+    id: 'person',
+    columns: [{
+      name: 'id',
+      primaryKey: true,
+    }, {
+      name: 'firstName',
+    }, {
+      name: 'lastName',
+    }]
+  })
+
+  var session = messages.createSession({
+    filter: [{
+      field: 'id',
+      comparison: 'gt',
+      value: 1
+    }],
+    sort: [{ field: 'id', direction: 'ASC' }],
+  })
+
+  let updated$ = fromEvent(session, 'dataUpdate')
+    .pipe(map(([d]) => d))
+
+
+  tapeAssert(t, updated$, result.map(flat))
+    .subscribe(
+      () => t.end(),
+      e => { console.trace(e) }
+    )
+
+  messages.add({id: 1, firstName: 'John', lastName: 'Doe'})
+  messages.add({id: 2, firstName: 'Richard', lastName: 'Stallman'})
+  messages.add({id: 3, firstName: 'Linus', lastName: 'Torvalds'})
+
+  session.updateColumns([
+    { name: 'id', primaryKey: true },
+    { name: 'firstName' }
+  ])
+})
