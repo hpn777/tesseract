4480c53d55c4aa583bfc59ce0dac7b0ee7d37f0f
Rafal Okninski <hpn777@gmail.com>
2020-05-02 03:03:07 +0100

    creating package for UI

---
+++ export.js
@@ -0,0 +1,9 @@
+let EventHorizon = require('./lib/eventHorizon')
+let Tesseract = require('./lib/tesseract')
+let Backbone = require('./lib/dataModels/backbone')
+let ExpressionEngine = require('./lib/expressionEngine')
+let _ = require('lodash')
+let Enumerable = require('linq')
+let tessioUtils = require('./lib/utils')
+
+module.exports = {EventHorizon, Tesseract, ExpressionEngine, Backbone, _, Enumerable, tessioUtils}
