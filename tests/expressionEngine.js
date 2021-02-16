9d4c642fda47b33b8ccdf69feff6cbb5aabce278
Rafal Okninski <hpn777@gmail.com>
2021-02-16 02:59:44 +0000

    expression engine fixes

--- tests/expressionEngine.js
+++ tests/expressionEngine.js
@@ -1,9 +1,9 @@
  const { ExpressionEngine } = require('../lib/expressionEngine')
 
 let ee = new ExpressionEngine()
-let expression = ee.generateExpressionTree('a in (1,5,8)')
-let expression2 = ee.generateExpressionTree('(a + b) > (a-b)')
+let expression = ee.generateExpressionTree('a in (1 ,5 , 8 )')
+let expression2 = ee.generateExpressionTree('nomi == 1')
 let expression3 = ee.generateExpressionTree('a notin (1,5,8)')
-console.log(JSON.stringify(expression3, null, 2))
+console.log(expression, expression.ToString())
 // console.log(ee.executeExpressionTree(expression, {a: 2}))
-console.log(ee.executeExpressionTree(expression3, {a: 5}))
+// console.log(ee.executeExpressionTree(expression3, {a: 5}))
