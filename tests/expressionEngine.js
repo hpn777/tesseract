3c6a55b5912949bdd47da0ec1b289030a8fd05b2
Rafal Okninski <hpn777@gmail.com>
2021-02-16 11:47:49 +0000

    expressionEngine.js fixes

--- tests/expressionEngine.js
+++ tests/expressionEngine.js
@@ -1,9 +1,9 @@
  const { ExpressionEngine } = require('../lib/expressionEngine')
 
 let ee = new ExpressionEngine()
-let expression = ee.generateExpressionTree('a in (1 ,5 , 8 )')
+let expression = ee.generateExpressionTree('dupa.toLinq().where(x=>x == 3).toArray()')
 let expression2 = ee.generateExpressionTree('nomi == 1')
 let expression3 = ee.generateExpressionTree('a notin (1,5,8)')
 console.log(expression, expression.ToString())
-// console.log(ee.executeExpressionTree(expression, {a: 2}))
+console.log(ee.executeExpressionTree(expression, {dupa: [1,2,3,4,5]}))
 // console.log(ee.executeExpressionTree(expression3, {a: 5}))
